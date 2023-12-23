97ca50339aa9fc65b4f1dd66255ec72c50f07ccc
Владимир Цепенщиков <58182585+QimbE@users.noreply.github.com>
2023-12-23 13:09:53 +0500

    App services and pipeline configuration (#3)
    * chore: configure DI
    
    * chore: configure main project startup
    
    * fix: move part of pipeline to TODO

--- AudioBaseAPI/Program.cs
+++ AudioBaseAPI/Program.cs
@@ -1,3 +1,9 @@
+using Application;
+using Carter;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
 namespace AudioBaseAPI;
 
 public class Program
@@ -6,22 +12,59 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services
+            .AddApplication()
+            .AddInfrastructure()
+            .AddPresentation();
+        
         builder.Services.AddEndpointsApiExplorer();
+        
         builder.Services.AddSwaggerGen();
 
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration));
+        
+        builder.Services.AddCors();
+
+        
+        
         var app = builder.Build();
 
+        //TODO: Add Global exception handling
+        
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        
+        //Cors policies
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+        }
+        else
+        {
+            app.UseCors(x => x
+                .WithOrigins(
+                    app.Configuration.GetValue<string[]>("Cors:AllowedHosts")!
+                    )
+            );
+        }
 
         app.UseHttpsRedirection();
+        
+        //TODO: app.UseAuthentication(); app.UseAuthorization();
 
+        app.UseSerilogRequestLogging();
+
+        //TODO: app.MapCarter(); app.MapGraphQL();
+        
         app.Run();
     }
 }
