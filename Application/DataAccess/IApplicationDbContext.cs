ad39af776d9a127b134fc1dcf76546928df0880d
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-23 23:02:11 +0500

    feat: add db context

---
+++ Application/DataAccess/IApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.DataAccess;
+
+public interface IApplicationDbContext
+{
+    
+}
---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Application.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ApplicationDbContext: DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
