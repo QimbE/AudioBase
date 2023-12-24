e2d2a56eb8b40c1e533d8913dbc76d09e4cb8ba5
Владимир Цепенщиков <vovar3390@gmail.com>
2023-12-24 17:53:03 +0500

    tests: new entities unit tests

---
+++ Tests/DomainTests/RefreshTokenTests.cs
@@ -0,0 +1,36 @@
+using Domain.Users;
+using FluentAssertions;
+using Throw;
+
+namespace DomainTests;
+
+public class RefreshTokenTests
+{
+    [Fact]
+    public void Create_ShouldThrowException_OnNullOrWhiteSpaceValue()
+    {
+        // Arrange
+        var action = () => RefreshToken.Create("    ", Guid.NewGuid());
+        
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Update_ShouldNotThrowException_OnValidValue()
+    {
+        // Arrange
+        var token = RefreshToken.Create("random123", Guid.NewGuid());
+        var validString = "SomeValidString";
+        
+        var action = () =>
+        {
+            token.Update(validString);
+        };
+
+        // Assert
+        action.Should().NotThrow<ArgumentException>();
+
+        token.Value.Should().BeEquivalentTo(validString);
+    }
+}
---
+++ Tests/DomainTests/UserTests.cs
@@ -0,0 +1,67 @@
+﻿using Domain.Users;
+using FluentAssertions;
+
+namespace DomainTests;
+
+public class UserTests
+{
+    [Fact]
+    public void Create_ShouldThrowException_OnNullOrWhiteSpaceName()
+    {
+        // Arrange
+        var action = () => User.Create("  ", "123", "blablabla", 1);
+        
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+    
+    [Fact]
+    public void Create_ShouldThrowException_OnNullOrWhiteSpaceEmail()
+    {
+        // Arrange
+        var action = () => User.Create("123", "  ", "blablabla", 1);
+        
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+    
+    [Fact]
+    public void Create_ShouldThrowException_OnNullOrWhiteSpacePassword()
+    {
+        // Arrange
+        var action = () => User.Create("123", "blablabla", "  ", 1);
+        
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+    
+    [Fact]
+    public void Create_ShouldThrowException_OnNonexistentRoleId()
+    {
+        // Arrange
+        var action = () => User.Create("123", "blablabla", "12345", -123341);
+        
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+    
+    [Fact]
+    public void Update_ShouldNotThrowException_OnValidInput()
+    {
+        // Arrange
+        var user = User.Create("123", "blablabla", "12345", Role.List.First());
+        var expectedUser = User.Create("test", "test", "test", Role.List.First());
+        
+        var action = () => user.Update("test", "test", "test", Role.List.First());
+        
+        // Assert
+        action.Should().NotThrow<ArgumentException>();
+
+        user.Should().Match<User>(u =>
+            u.Name == expectedUser.Name &&
+            u.Email == expectedUser.Email && 
+            u.Password == expectedUser.Password &&
+            u.RoleId == expectedUser.RoleId
+            );
+    }
+}
