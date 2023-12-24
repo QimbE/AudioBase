1f261cf9ec118a72c4a63c7d62912f110064f087
Владимир Цепенщиков <vovar3390@gmail.com>
2023-12-24 12:54:10 +0500

    feat: add user entity

---
+++ Domain/Users/User.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.Json.Serialization;
+using Domain.Abstractions;
+using Throw;
+
+namespace Domain.Users;
+
+/// <summary>
+/// Application user
+/// </summary>
+public class User
+    : Entity
+{
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        protected set
+        {
+            _name = value.Throw().IfNullOrWhiteSpace(x => x);
+        }
+    }
+
+    private string _email;
+
+    public string Email
+    {
+        get => _email;
+        protected set
+        {
+            _email = value.Throw().IfNullOrWhiteSpace(x => x);
+        }
+    }
+
+    private string _password;
+
+    /// <summary>
+    /// Password hash
+    /// </summary>
+    [JsonIgnore]
+    public string Password
+    {
+        get => _password;
+        protected set
+        {
+            _password = value.Throw().IfNullOrWhiteSpace(x => x);
+        } }
+
+    private int _roleId;
+
+    public int RoleId
+    {
+        get => _roleId;
+        protected set
+        {
+            // if there is no such a role
+            Role.List.Throw().IfNotContains(Role.FromValue(value));
+            _roleId = value;
+        }
+    }
+
+    public Role Role { get; protected set; }
+    
+    public Guid RefreshTokenId { get; protected set; }
+
+    public RefreshToken RefreshToken { get; protected set; }
+
+    protected User()
+    {
+        
+    }
+
+    protected User(string name, string email, string password, int roleId, Guid refreshTokenId)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        RoleId = roleId;
+        RefreshTokenId = refreshTokenId;
+    }
+
+    public User Create(string name, string email, string password, int roleId, Guid refreshTokenId)
+    {
+        return new(name, email, password, roleId, refreshTokenId);
+    }
+
+    public void Update(string name, string email, string password, int roleId, Guid refreshTokenId)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        RoleId = roleId;
+        RefreshTokenId = refreshTokenId;
+    }
+}
