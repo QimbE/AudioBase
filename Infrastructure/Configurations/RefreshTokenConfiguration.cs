17009ffa15c688f326b5de930c2b673a355245b0
Владимир Цепенщиков <vovar3390@gmail.com>
2023-12-24 15:36:10 +0500

    feat: add ef core configurations

---
+++ Infrastructure/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class RefreshTokenConfiguration: IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.HasIndex(r => r.Value);
+
+        //TODO: perhaps add max length
+        builder.Property(r => r.Value).IsRequired();
+
+        builder.Property(r => r.ExpirationDate).IsRequired();
+    }
+}
---
+++ Infrastructure/Configurations/RoleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasKey(r => r.Value).HasName("Id");
+
+        builder.HasIndex(r => r.Name).IsUnique();
+
+        builder.Property(r => r.Name).IsRequired().HasMaxLength(50);
+        
+        builder.HasData(Role.List);
+    }
+}
