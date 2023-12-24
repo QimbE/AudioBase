e58ae1efb4a55d6b028a33daac32b8f2a9d9c3ab
Владимир Цепенщиков <vovar3390@gmail.com>
2023-12-24 15:53:34 +0500

    feat: add entities to db context

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,29 @@
 ﻿using Application.DataAccess;
+using Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
+/// <summary>
+/// Main AudioBase Db context
+/// </summary>
 public class ApplicationDbContext: DbContext, IApplicationDbContext
 {
+    /// <summary>
+    /// Application users
+    /// </summary>
+    public DbSet<User> Users { get; set; }
+    
+    /// <summary>
+    /// User roles
+    /// </summary>
+    public DbSet<Role> Roles { get; set; }
+    
+    /// <summary>
+    /// User access refresh tokens
+    /// </summary>
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         :base(options)
     {
