393ba831c9491f7e19695db54c49e2d032fa9918
QimbE <58182585+QimbE@users.noreply.github.com>
2023-12-28 23:15:55 +0500

    feat: register authentication and authorization

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,14 +1,18 @@
-﻿using Application.Behaviors;
+﻿using System.Text;
+using Application.Behaviors;
 using Application.ExceptionHandlers;
 using Application.GraphQL;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
         var assembly = typeof(DependencyInjection).Assembly;
 
@@ -26,6 +30,32 @@ public static class DependencyInjection
         services.AddGraphQLServer()
             .ConfigureHotChocolateTypes()
             .ConfigurePipeline();
+        
+        // Authentication
+        var secretKey = Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]!);
+        
+        var tokenValidationParameter = new TokenValidationParameters
+        {
+            ValidIssuer = configuration["JwtSettings:Issuer"],
+            ValidAudience = configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = tokenValidationParameter;
+        });
+        
+        services.AddAuthorization();
         
         return services;
     }
