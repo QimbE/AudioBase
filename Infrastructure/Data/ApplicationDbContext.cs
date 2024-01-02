49fc9485d5253b88b9bf996b10a5a430dd126565
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-02 21:36:01 +0500

    feat: add new table to db

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DataAccess;
 using Domain.Users;
+using Infrastructure.Outbox;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
@@ -23,6 +24,11 @@ public class ApplicationDbContext: DbContext, IApplicationDbContext
     /// User access refresh tokens
     /// </summary>
     public DbSet<RefreshToken> RefreshTokens { get; set; }
+    
+    /// <summary>
+    /// Integration and domain event list.
+    /// </summary>
+    public DbSet<OutboxMessage> OutboxMessages { get; set; }
     
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         :base(options)
