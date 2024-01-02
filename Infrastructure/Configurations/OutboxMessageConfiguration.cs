7a5fe6b4b2d26e68d38472e7ce7076f8782dc59f
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-02 20:52:47 +0500

    feat: add outbox message and configuration

---
+++ Infrastructure/Configurations/OutboxMessageConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class OutboxMessageConfiguration: IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.ToTable("OutboxMessages");
+    }
+}
---
+++ Infrastructure/Outbox/OutboxMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Outbox;
+
+public sealed record OutboxMessage(
+    Guid Id,
+    string Name,
+    string Content,
+    DateTime CreatedOnUtc,
+    DateTime? ProcessedOnUtc,
+    string? Error
+    );
