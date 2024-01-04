e0146bf65064954a3c288e7d3f221161cefe88d6
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-04 23:50:22 +0500

    tests: add base class for authentication tests

---
+++ Tests/ApplicationTests/Authentication/AuthTestingBase.cs
@@ -0,0 +1,43 @@
+﻿using Application.Authentication;
+using Infrastructure.Authentication;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+
+namespace ApplicationTests.Authentication;
+
+public abstract class AuthTestingBase
+{
+    protected readonly TestDbContext Context;
+    protected readonly IJwtProvider JwtProvider;
+    protected readonly IHashProvider HashProvider;
+    
+    public AuthTestingBase()
+    {
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        builder
+            .UseInMemoryDatabase("Test");
+
+        Context = new TestDbContext(builder.Options);
+        
+        var sectionMock = Substitute.For<IConfigurationSection>();
+
+        sectionMock["Key"].Returns("hehehehuhhehehehehehehehuhhwhwhwhhh");
+        sectionMock["Issuer"].Returns("huh");
+        sectionMock["Audience"].Returns("bimbimbambam");
+        sectionMock["ExpiryTime"].Returns("00:30:00");
+        
+        var configMock = Substitute.For<IConfiguration>();
+
+        configMock.GetSection("JwtSettings").Returns(sectionMock);
+
+        JwtProvider = new JwtProvider(configMock);
+        HashProvider = new HashProvider();
+    }
+    
+    ~AuthTestingBase()
+    {
+        Context.Dispose();
+    }
+}
