a6a9645a888eca3db44fd88361f12a242e4effd0
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-05 23:01:36 +0500

    feat: add background job for handling outbox messages

---
+++ Infrastructure/BackgroundJobs/ProcessOutboxMessagesJob.cs
@@ -0,0 +1,63 @@
+﻿using Domain.Abstractions;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Quartz;
+
+namespace Infrastructure.BackgroundJobs;
+
+// TODO: Probably we'll have to extract the whole background job service in separate project/solution
+[DisallowConcurrentExecution]
+public class ProcessOutboxMessagesJob: IJob
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IPublisher _publisher;
+    private readonly ILogger<ProcessOutboxMessagesJob> _logger;
+
+    public const int MaximumMessagesForOneJob = 20;
+
+    public ProcessOutboxMessagesJob(ApplicationDbContext context, IPublisher publisher, ILogger<ProcessOutboxMessagesJob> logger)
+    {
+        _context = context;
+        _publisher = publisher;
+        _logger = logger;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        // Unprocessed messages
+        var messages = await _context.OutboxMessages
+            .Where(m => m.ProcessedOnUtc == null)
+            .Take(MaximumMessagesForOneJob)
+            .ToListAsync(context.CancellationToken);
+
+        foreach (var message in messages)
+        {
+            // Something tells me that it is actually Integration, not Domain event (but who cares?)
+            var domainEvent = JsonConvert
+                .DeserializeObject<DomainEvent>(message.Content);
+
+            // Pretty bad scenario, because we can't even debug to know what happend
+            if (domainEvent is null)
+            {
+                _logger.LogWarning(
+                    "Outbox message {@name} with Id: {@id} turned out to contain null event.",
+                    message.Name,
+                    message.Id
+                    );
+
+                message.Error = "Something went wrong, while deserializing content of this message";
+                message.ProcessedOnUtc = DateTime.UtcNow;
+                continue;
+            }
+
+            await _publisher.Publish(domainEvent, context.CancellationToken);
+            
+            message.ProcessedOnUtc = DateTime.UtcNow;
+        }
+
+        await _context.SaveChangesAsync(context.CancellationToken);
+    }
+}
