d3244156b801fd276f38dad5d9515e522c0979c4
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-05 14:30:04 +0500

    tests: add logout command handler tests

---
+++ Tests/ApplicationTests/Authentication/LogoutCommandHandlerTests.cs
@@ -0,0 +1,83 @@
+﻿using Application.Authentication.Logout;
+using Application.Authentication.Refresh;
+using Domain.Users;
+using Domain.Users.Exceptions;
+using FluentAssertions;
+
+namespace ApplicationTests.Authentication;
+
+public class LogoutCommandHandlerTests: AuthTestingBase
+{
+    public static IEnumerable<object[]> InvalidUsersAndTokens()
+    {
+        var user1 = User.Create("123", "test", "123123", 1);
+        
+        // Expired token
+        var expiredToken = RefreshToken.Create("123123", user1.Id);
+        expiredToken.MakeExpire();
+        
+        // Token that does not exist
+        var nonExistentToken = RefreshToken.Create("123", Guid.NewGuid());
+        
+        return [[user1, expiredToken], [user1, nonExistentToken]];
+    }
+    
+    [Theory]
+    [MemberData(nameof(InvalidUsersAndTokens))]
+    public async Task Logout_ShouldReturnException_OnInvalidToken(User user, RefreshToken token)
+    {
+        // Arrange
+        await RecreateDbContextAsync();
+        Context.Users.Add(user);
+        
+        if (token.Id == user.Id)
+        {
+            Context.RefreshTokens.Add(token);
+        }
+
+        await Context.SaveChangesAsync();
+
+        var handler = new LogoutCommandHandler(Context);
+
+        var request = new LogoutCommand(token.Value);
+        
+        // Act
+        var res = await handler.Handle(request, default);
+        
+        // Assert
+        res.IsFaulted.Should().BeTrue();
+
+        var exception = res.Match<Exception>(
+            success => null,
+            failure => failure
+        );
+
+        exception.Should().BeOfType<InvalidRefreshTokenException>();
+    }
+    
+    [Fact]
+    public async Task Refresh_ShouldReturnTokenResponse_OnValidToken()
+    {
+        // Arrange
+        await RecreateDbContextAsync();
+        
+        var user = User.Create("123", "test", "123123", 1);
+
+        var token = RefreshToken.Create("123123123123", user.Id);
+        
+        Context.Users.Add(user);
+        Context.RefreshTokens.Add(token);
+        
+        await Context.SaveChangesAsync();
+
+        var handler = new LogoutCommandHandler(Context);
+
+        var request = new LogoutCommand(token.Value);
+        
+        // Act
+        var res = await handler.Handle(request, default);
+        
+        // Assert
+        res.IsSuccess.Should().BeTrue();
+    }
+}
