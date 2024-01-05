dd4f330cf7fb74d608e501e18284412367a6d0ad
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-05 21:33:41 +0500

    fix: make outbox message mutable

--- Infrastructure/Outbox/OutboxMessage.cs
+++ Infrastructure/Outbox/OutboxMessage.cs
@@ -1,10 +1,32 @@
 ﻿namespace Infrastructure.Outbox;
 
-public sealed record OutboxMessage(
-    Guid Id,
-    string Name,
-    string Content,
-    DateTime CreatedOnUtc,
-    DateTime? ProcessedOnUtc = null,
-    string? Error = null
-    );
+/// <summary>
+/// A part of implementation of transactional outbox pattern.
+/// </summary>
+/// <param name="id">Id of this message</param>
+/// <param name="name">Name of event, that produced this message</param>
+/// <param name="content">Json with actual event to handle</param>
+/// <param name="createdOnUtc">Time when was created</param>
+/// <param name="processedOnUtc">Time when was processed</param>
+/// <param name="error">Some error while handling (if occured)</param>
+public sealed class OutboxMessage(
+    Guid id,
+    string name,
+    string content,
+    DateTime createdOnUtc,
+    DateTime? processedOnUtc = null,
+    string? error = null
+)
+{
+    public Guid Id { get; init; } = id;
+    
+    public string Name { get; init; } = name;
+    
+    public string Content { get; init; } = content;
+    
+    public DateTime CreatedOnUtc { get; init; } = createdOnUtc;
+    
+    public DateTime? ProcessedOnUtc { get; set; } = processedOnUtc;
+
+    public string? Error { get; set; } = error;
+}
