756386b5c6fa162f73a2bf77c26523881780f04e
Stephen <sokhrannykhs@gmail.com>
2024-01-05 15:17:10 +0500

    feat: add genre entity

---
+++ Domain/Catalog/Genre.cs
@@ -0,0 +1,45 @@
+using Domain.Abstractions;
+using Throw;
+
+namespace Domain.Catalog;
+
+/// <summary>
+/// Genre of a track
+/// </summary>
+/// <param name="Id"> Inherited from base "Entity" class </param>
+/// <param name="_name"> Name of the genre </param>
+public class Genre
+    : Entity<Genre>
+{
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        protected set
+        {
+            _name = value.Throw().IfNullOrWhiteSpace(x => x);
+        }
+    }
+
+    protected Genre()
+    {
+        
+    }
+    
+    // ID generates via method from the base class
+    protected Genre(string name)
+        : base(Guid.NewGuid())
+    {
+        Name = name;
+    }
+
+    public static Genre Create(string name)
+    {
+        return new(name);
+    }
+
+    public void Update(string name)
+    {
+        Name = name;
+    }
+}
