7cf90693a3ff363a596cce7129c2d172b50d8b7e
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-06 00:52:15 +0500

    feat: add outbox message consumer

---
+++ Infrastructure/Configurations/OutboxMessageConsumerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class OutboxMessageConsumerConfiguration: IEntityTypeConfiguration<OutboxMessageConsumer>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessageConsumer> builder)
+    {
+        builder.HasKey(c => new {c.Id, c.ConsumerName});
+
+        builder.Property(c => c.ConsumerName).IsRequired();
+    }
+}
---
+++ Infrastructure/Outbox/OutboxMessageConsumer.cs
@@ -0,0 +1,11 @@
+﻿using Throw;
+
+namespace Infrastructure.Outbox;
+
+public sealed class OutboxMessageConsumer(Guid id, string name)
+{
+    public Guid Id { get; init; } = id.ThrowIfNull();
+
+    public string ConsumerName { get; init; } = name.Throw()
+        .IfNullOrWhiteSpace(x => x);
+}
