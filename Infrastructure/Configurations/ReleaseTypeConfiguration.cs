77cd15ab369c34dc073d5c96576b17a8fda46b41
Stephen <sokhrannykhs@gmail.com>
2024-01-06 19:12:59 +0500

    feat: add release type configuration

---
+++ Infrastructure/Configurations/ReleaseTypeConfiguration.cs
@@ -0,0 +1,21 @@
+using Domain.MusicReleases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class ReleaseTypeConfiguration : IEntityTypeConfiguration<ReleaseType>
+{
+    public void Configure(EntityTypeBuilder<ReleaseType> builder)
+    {
+        // Release type is SmartEmum, therefore definition is different
+        builder.HasKey(rt => rt.Value).HasName("Id");
+
+        builder.HasIndex(rt => rt.Name).IsUnique();
+
+        builder.Property(rt => rt.Name).IsRequired().HasMaxLength(50);
+        
+        // Returns whole list of release types
+        builder.HasData(ReleaseType.List);
+    }
+}
