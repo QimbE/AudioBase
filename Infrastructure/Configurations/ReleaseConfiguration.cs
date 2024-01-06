5a2615ca6fc393f032393f616ed0e8e4ca21668c
Stephen <sokhrannykhs@gmail.com>
2024-01-06 19:05:00 +0500

    feat: add release configuration

---
+++ Infrastructure/Configurations/ReleaseConfiguration.cs
@@ -0,0 +1,23 @@
+using Domain.MusicReleases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class ReleaseConfiguration : IEntityTypeConfiguration<Release>
+{
+    public void Configure(EntityTypeBuilder<Release> builder)
+    {
+        builder.HasKey(r => r.Id);
+        
+        builder.Property(r => r.Name).HasMaxLength(50);
+        
+        builder.Property(r => r.CoverLink);
+        
+        builder.Property(r => r.AuthorId);
+        
+        builder.Property(r => r.ReleaseTypeId);
+        
+        builder.Property(r => r.ReleaseDate);
+    }
+}
