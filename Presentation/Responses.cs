39ca451a6aa4b3bd536c886380ccb73ae874e1bf
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-06 23:34:43 +0500

    feat: add API responses

---
+++ Presentation/Responses.cs
@@ -0,0 +1,28 @@
+﻿namespace Presentation;
+
+/// <summary>
+/// Base success response (No data attached)
+/// </summary>
+public class BaseResponse
+{
+    public string Message { get; protected set; } = "Success";
+    
+    public BaseResponse()
+    {
+        Message = "Success";
+    }
+}
+
+/// <summary>
+/// Response with any data
+/// </summary>
+public class ResponseWithData : BaseResponse
+{
+    public object Data { get; protected set; }
+
+    public ResponseWithData(object data)
+        : base()
+    {
+        Data = data;
+    }
+}
