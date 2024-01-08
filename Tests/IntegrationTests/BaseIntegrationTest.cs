ab566cdde77f439dacb9c7057a423117be774b70
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-08 21:16:14 +0500

    chore: add integration tests project

---
+++ Tests/IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests;
+
+public abstract class BaseIntegrationTest
+    : IClassFixture<IntegrationTestWebAppFactory>
+{
+    protected readonly IntegrationTestWebAppFactory Factory;
+    
+    public BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+    {
+        Factory = factory;
+    }
+}
