4a53106352efb65d4d314fb63a64f0c78ab2b628
Владимир Цепенщиков <58182585+QimbE@users.noreply.github.com>
2024-01-18 12:54:49 +0500

    Add change role endpoint (#28)
    * feat: add new domain exceptions
    
    * feat: add change role command
    
    * tests: add unit tests
    
    * fix: command validation
    
    * feat: add notfound mappint to mapper
    
    * feat: add update role endpoint
    
    * tests: add core authentication logic integration tests
    
    * tests: add change role endpoint tests
    
    * fix: endpoint docs

---
+++ Domain/Abstractions/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Abstractions.Exceptions;
+
+public abstract class NotFoundException: Exception
+{
+    public NotFoundException(string message)
+        : base(message)
+    {
+        
+    }
+}
+
+public abstract class EntityNotFoundException<T> : NotFoundException
+    where T: Entity
+{
+    public EntityNotFoundException(Guid id)
+        : base($"Entity of type {typeof(T)} with Id {id} was not found in database")
+    {
+        
+    }
+}
