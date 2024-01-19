408b1f7daac338e8a65a584401d0d4fe7029cec3
QimbE <58182585+QimbE@users.noreply.github.com>
2024-01-20 01:50:49 +0500

    feat: new domain exception

---
+++ Domain/Users/Exceptions/ChangeToTheSamePasswordException.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Domain.Users.Exceptions;
+
+public class ChangeToTheSamePasswordException: DuplicateNameException
+{
+    public ChangeToTheSamePasswordException()
+        : base("Your new password must not be the same as previous one")
+    {
+        
+    }
+}
