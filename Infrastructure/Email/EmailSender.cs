9aa064a8ffdfd8fed3b3d6d96137e2fd2fa4e5af
Владимир Цепенщиков <vovar3390@gmail.com>
2024-03-01 23:42:04 +0500

    feat + refactor: add new method to IEmailSender

--- Infrastructure/Email/EmailSender.cs
+++ Infrastructure/Email/EmailSender.cs
@@ -18,7 +18,6 @@ public class EmailSender: IEmailSender
     private readonly IJwtProvider _jwtProvider;
     private readonly SmtpClient _client;
 
-    private const string VerificationMailSubject = "AudioBase Email verification";
     public EmailSender(IOptionsMonitor<EmailSettings> settings, IJwtProvider jwtProvider, SmtpClient client)
     {
         _jwtProvider = jwtProvider;
@@ -28,23 +27,43 @@ public class EmailSender: IEmailSender
     
     public async Task SendVerificationEmail(User user)
     {
+        var (subject, body) = EmailMessageBodies.GetVerificationEmailContent(
+            user.Name,
+            _settings.VerificationPageUrl,
+            await _jwtProvider.GenerateVerificationToken(user)
+            );
+        var email = GetMessage(
+            _settings.DisplayName,
+            _settings.From,
+            user.Email,
+            subject,
+            body
+            );
+        SendMessage(email);
+    }
+
+    public async Task SendChangePasswordEmail(User user, string newPassword)
+    {
+        var (subject, body) = EmailMessageBodies.GetChangePasswordEmailContent(
+            user.Name,
+            newPassword
+        );
+        var email = GetMessage(
+            _settings.DisplayName,
+            _settings.From,
+            user.Email,
+            subject,
+            body
+        );
+        SendMessage(email);
+    }
+
+    private void SendMessage(MimeMessage email)
+    {
         if (!_settings.IsProduction)
         {
             return;
         }
-
-        var email = new MimeMessage();
-        email.From.Add(new MailboxAddress(_settings.DisplayName, _settings.From));
-        email.To.Add(MailboxAddress.Parse(user.Email));
-        email.Subject = VerificationMailSubject;
-        email.Body = new TextPart(TextFormat.Html)
-        {
-            Text = EmailMessageBodies.GetVerificationEmailBody(
-                user.Name,
-                _settings.VerificationPageUrl, 
-                await _jwtProvider.GenerateVerificationToken(user)
-                )
-        };
         
         _client.Connect(_settings.Host, _settings.Port, SecureSocketOptions.SslOnConnect);
         _client.AuthenticationMechanisms.Remove("XOAUTH2");
@@ -52,11 +71,28 @@ public class EmailSender: IEmailSender
         
         _client.Send(email);
     }
+
+    protected static MimeMessage GetMessage(string displayName, string from, string addressee, string subject, string messageBody)
+    {
+        var email = new MimeMessage();
+        email.From.Add(new MailboxAddress(displayName, from));
+        email.To.Add(MailboxAddress.Parse(addressee));
+        email.Subject = subject;
+        email.Body = new TextPart(TextFormat.Html)
+        {
+            Text = messageBody
+        };
+
+        return email;
+    }
 }
 
 public static class EmailMessageBodies
 {
-    // TODO: Make cool verification letter (optionally)
-    public static string GetVerificationEmailBody(string userName, string verificationEndpointLink, string verificationToken) =>
-        $"<p>Dear {userName}!</p> <p>Please verify your email address by clicking here:</p> <a href=\"{verificationEndpointLink}?token={verificationToken}\">link</a>";
+    // TODO: Make cool letters (optionally)
+    public static (string, string) GetVerificationEmailContent(string userName, string verificationEndpointLink, string verificationToken) =>
+        ("AudioBase Email verification", $"<p>Dear {userName}!</p> <p>Please verify your email address by clicking here:</p> <a href=\"{verificationEndpointLink}?token={verificationToken}\">link</a>");
+    
+    public static (string, string) GetChangePasswordEmailContent(string userName, string newPassword) =>
+        ("AudioBase Email verification", $"<p>Dear {userName}!</p> <p>We have received a request to change password for the account with this email. Your new password is {newPassword}</p> ");
 }
